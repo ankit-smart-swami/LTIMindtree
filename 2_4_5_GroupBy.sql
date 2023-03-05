d95aff7bcef208958ed5c2620f37189bc807d671
ankit-smart-swami <83894723+ankit-smart-swami@users.noreply.github.com>
2023-03-05 11:54:22 +0530

    Update 2_4_5_GroupBy.sql

--- 2_4_5_GroupBy.sql
+++ 2_4_5_GroupBy.sql
@@ -1,40 +1,44 @@
 --Query-1
-
+SELECT batch, COUNT(id) AS profile_count
+FROM profile GROUP BY batch
+ORDER BY batch ASC;
 
 ---------------------------------------------
 --Query-2
-
+SELECT batch, COUNT(id) 
+AS male_alumni_count
+FROM profile 
+WHERE gender = 'male'
+GROUP BY batch
+ORDER BY batch ASC;
 
 ---------------------------------------------
 --Query-3
-
+SELECT batch, COUNT(id) 
+AS female_alumni_count
+FROM profile 
+WHERE gender = 'female'
+GROUP BY batch
+ORDER BY batch ASC;
 
 ---------------------------------------------
 --Query-4
-
+SELECT designation, 
+COUNT(id) AS designation_count
+FROM profile GROUP BY designation
+ORDER BY designation ASC;
 
 ---------------------------------------------
 --Query-5
-
+SELECT YEAR(date), 
+COUNT(id) AS number_of_events
+FROM event GROUP BY YEAR(date)
+ORDER BY YEAR(date);
 
 ---------------------------------------------
 --Query-6
-
-
----------------------------------------------
---Query-7
-
-
----------------------------------------------
---Query-8
-
-
----------------------------------------------
---Query-9
-
-
----------------------------------------------
---Query-10
-
+SELECT month, 
+min(total_units) AS minimum_units
+FROM bill GROUP BY month;
 
 ---------------------------------------------
