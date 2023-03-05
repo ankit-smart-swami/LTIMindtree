2a20be9c171972a554dc333e71c23acea3dbef26
ankit-smart-swami <83894723+ankit-smart-swami@users.noreply.github.com>
2023-03-05 15:18:03 +0530

    Update 2_4_7_Counting_records.sql

--- 2_4_7_Counting_records.sql
+++ 2_4_7_Counting_records.sql
@@ -4,19 +4,29 @@ role_count FROM role;
 
 ---------------------------------
 --Query-2
-
+SELECT COUNT(*) 
+AS skill_count FROM skill;
 
 ---------------------------------
 --Query-3
-
+SELECT COUNT(*) AS 
+user_count FROM user
+WHERE profile_id IS NULL;
 
 ---------------------------------
 --Query-4
-
+SELECT COUNT(*) 
+AS alumni_2008_count
+FROM profile
+WHERE batch = '2008';
 
 ---------------------------------
 --Query-5
-
+SELECT COUNT(*) 
+AS alumni_male_2008_count
+FROM profile
+WHERE batch = '2008'
+AND gender = 'male';
 
 ---------------------------------
 --Query-6
